7ad5e0e7ee69500ca30f89178b3f31179586d6fc
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-05-26 03:33:07 -0600

    Create storeList component

---
+++ src/components/storeList.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState } from "react";
+import StoreItem from "./storeItem";
+
+const StoreList = () => {
+  const [pointer, setPointer] = useState(0);
+
+  return (
+    <div>
+      <h2 className="text-[20px] mt-3 font-bold mb-3 flex items-center justify-between">
+        Top nearby stores
+        <span className="flex">
+          {pointer > 0 ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              onClick={() => setPointer(pointer - 1)}
+              className="w-10 h-10 p-2 text-gray-600 hover:text-pink-500 hover:bg-pink-100 cursor-pointer rounded-lg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          ) : null}
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            onClick={() => setPointer(pointer + 1)}
+            className="w-10 h-10 p-2 text-gray-600 hover:text-pink-500 hover:bg-pink-100 cursor-pointer rounded-lg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </span>
+      </h2>
+      <div>
+        <StoreItem />
+        <StoreItem />
+        <StoreItem />
+      </div>
+    </div>
+  );
+};
+
+export default StoreList;
